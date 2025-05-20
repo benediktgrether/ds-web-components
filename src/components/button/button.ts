d3889e4e87d56880b0afb578f68cacfaaff674d0
Benedikt Grether <info@benediktgrether.de>
2025-05-20 16:05:16 +0200

    first commit

---
+++ src/components/button/button.ts
@@ -0,0 +1,16 @@
+import { LitElement, html,unsafeCSS } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import style from './button.css';
+
+@customElement('ds-button')
+export class DSButton extends LitElement {
+
+    render() {
+        return html`
+            <button class="ds-button">
+                <slot></slot>
+            </button>
+        `;
+    }
+    static styles = unsafeCSS(style);
+}
