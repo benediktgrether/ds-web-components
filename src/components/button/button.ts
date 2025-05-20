0391559133b41e29d0547bb1f6b162e311746c43
Benedikt Grether <info@benediktgrether.de>
2025-05-20 16:14:07 +0200

    Add variant to ds button

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -1,13 +1,25 @@
 import { LitElement, html,unsafeCSS } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
+import { classMap } from 'lit-html/directives/class-map.js';
 import style from './button.css';
 
 @customElement('ds-button')
 export class DSButton extends LitElement {
 
+    @property({ type: String, reflect: true })
+    variant: 'primary' | 'secondary' = 'primary';
+
     render() {
+        const { variant } = this;
+
+        const buttonClasses = {
+            'ds-button': true,
+            'ds-button--primary': variant === 'primary',
+            'ds-button--secondary': variant === 'secondary',
+        };
+
         return html`
-            <button class="ds-button ds-button--primary">
+            <button class=${classMap(buttonClasses)}>
                 <slot></slot>
             </button>
         `;
