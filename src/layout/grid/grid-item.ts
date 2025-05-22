d064e9ef7a3c2356388500f28d5f09e20fa4a64b
Benedikt <info@benediktgrether.de>
2025-05-22 23:38:06 +0200

    Add gap-y

--- src/layout/grid/grid-item.ts
+++ src/layout/grid/grid-item.ts
@@ -1,12 +1,19 @@
 import { LitElement, html, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { classMap } from 'lit-html/directives/class-map.js';
 import style from '@css/main.css';
 
 @customElement('ds-grid-item')
 export class DSGridItem extends LitElement {
+    @property({ type: String }) gap = '0'; // Default
+
     render() {
+        const classes = {
+            [`flex flex-col gap-y-${this.gap}`]: true,
+        };
+
         return html`
-            <div class="">
+            <div class=${classMap(classes)}>
                 <slot></slot>
             </div>
         `;
